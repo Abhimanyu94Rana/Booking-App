4512a9e88aeffc9d108c15829d52526ec169e7f2
Jatinder <Jatinder@shavik.ai>
2022-06-04 11:37:44 +0530

    Fix login issue

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {return view('login');})->name('login');
-Route::post('/login', [App\Http\Controllers\LoginController::class, 'postLogin'])->name('postLogin');
+Route::get('/login', function () {return view('login');});
+Route::post('/postLogin', [App\Http\Controllers\LoginController::class, 'postLogin'])->name('postLogin');
 Route::get('/logout', [App\Http\Controllers\LoginController::class, 'logout'])->name('logout');
 
 // Admin Routes
